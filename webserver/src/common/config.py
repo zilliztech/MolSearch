36ae63d9078429b1b1265b255f571d11f1bd3e48
shiyu22 <cshiyu22@gmail.com>
2020-12-08 11:42:31 +0800

    Update config

--- webserver/src/common/config.py
+++ webserver/src/common/config.py
@@ -4,10 +4,7 @@ MILVUS_HOST = os.getenv("MILVUS_HOST", "192.168.1.85")
 MILVUS_PORT = os.getenv("MILVUS_PORT", 19530)
 VECTOR_DIMENSION = os.getenv("VECTOR_DIMENSION", 2048)
 NUM = os.getenv("TOPK_NUM", 100)
-SIM_TABLE = os.getenv("SIM_TABLE", "similarity_table")
-SUB_TABLE = os.getenv("SUB_TABLE", "substructure_table")
-SUPER_TABLE = os.getenv("SUPER_TABLE", "superstructure_table")
-
+DEFULT_TABLE = os.getenv("DEFULT_TABLE", "molsearch")
 
 
 PG_HOST = os.getenv("PG_HOST", "192.168.1.85")
@@ -15,4 +12,5 @@ PG_PORT = os.getenv("PG_PORT", 5432)
 PG_USER = os.getenv("PG_USER", "postgres")
 PG_PASSWORD = os.getenv("PG_PASSWORD", "postgres")
 PG_DATABASE = os.getenv("PG_DATABASE", "postgres")
+PG_TABLE = os.getenv("PG_TABLE", "molsearch")
 PG_TABLE = os.getenv("PG_TABLE", "milvus")
